2c080edc7fb8d4a0a36bb16226c49a810da9fa51
RK Saykot <rksaikatkhan999@gmail.com>
2026-09-09 22:12:42 +0600

    Authentication issue solved

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.myroadmap.app',
   appName: 'MyRoadmap',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ["google.com"],
+    },
+  },
 };
 
 export default config;
